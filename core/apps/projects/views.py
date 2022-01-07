214a9ed2947e3f843c6a96fa5ebb3f9b4e096f2f
nirmalpandey048@gmail.com <nirmalpandey048@gmail.com>
2022-01-08 02:22:31 +0545

    create context processor for navbar categories

--- core/apps/projects/views.py
+++ core/apps/projects/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import redirect, render
 
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from core.apps.blog.models.category_models import Category
 class ProjectView(LoginRequiredMixin,View):
     
     def get(self,request,*args, **kwargs):
-        return render(request,'projects/project_list.html')
+        context = {
+          'title':'Projects'
+        }
+        return render(request,'projects/project_list.html',context)
         
---
+++ core/context_processor.py
@@ -0,0 +1,22 @@
+
+from core.apps.blog.models.category_models import Category
+from core.apps.blog.models.article_models import Article
+
+def categories(request):
+    categoriy_articles_count = []
+    
+    for category in Category.objects.values_list('pk', flat=True):#instead of all use this
+        category_instance = Category.objects.get(pk = category)
+        articles_count = category_instance.articles.filter(
+                                                            status = Article.PUBLISHED,
+                                                            deleted=False
+                                                            ).count()
+        categoriy_articles_count.append(articles_count)
+        
+    categories = zip(
+    Category.objects.filter(approved=True),#.values_list('name','slug','image'),
+    categoriy_articles_count
+                        )
+    return {
+        'categories':categories
+    }
